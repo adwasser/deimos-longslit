b21162227b3ad3a5982d829607b432a1f3c59f06
Asher Wasserman <asher@dhcp-67-101.ucsc.edu>
2015-12-16 16:40:37 -0800

    loop to remove overscan, need to mosaic

--- longslit.py
+++ longslit.py
@@ -7,6 +7,10 @@ Rough order of business:
     Trace and continuum subtraction
 '''
 
+import sys, re
+import numpy as np
+from astropy.io import fits
+
 def arrange_fits(fitsname, save_to=None):
     '''
     Takes the standard DEIMOS packaging of fits header data units (hdu's) and
@@ -22,6 +26,21 @@ def arrange_fits(fitsname, save_to=None):
     ------------
         :raw_data: 2d numpy array, rows are spectral, columns are spatial
     '''
+    # f is an HDU list, with f[0] being the primary HDU and f[1:9] being the
+    # image arrays
+    f = fits.open(fitsname)
+    data_arrays = []
+    # Everybody stand back.  I know regular expressions.
+    pattern  = re.compile('\[([0-9]+):([0-9]+),([0-9]+):([0-9]+)\]')
+    for i in range(1: len(f)):
+        y_size, x_size = f[i].shape
+        # get the usable pixels in the image, defined in the DATASEC keyword
+        datasec = f[i].header['DATASEC']
+        strings = pattern.match(datasec).group(1, 2, 3, 4)
+        x_low, x_high, y_low, y_high = map(float, strings)
+        # need to adjust the low values to account for zero-based indexing
+        x_low, y_low = x_low - 1., y_low - 1.
+        data_array.append(f[i].data[x_low: x_high, y_low: y_high])
     pass
 
 def normalize(data, flatdir="./flats", biasdir="./bias", save_to=None):
